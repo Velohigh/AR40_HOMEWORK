5c1cea2ce551a94a4558bbd0643f7582558ed413
Velohigh <85592598+Velohigh@users.noreply.github.com>
2022-01-17 05:49:52 +0900

    정건효 : 주말과제 ToUpper

---
+++ HomeWork220114_ToInt/HomeWork220114_ToInt.cpp
@@ -0,0 +1,31 @@
+﻿// HomeWork220114_ToInt.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 길이구하는 함수가 필요할듯..
+// 자리수에 
+
+int ToInt(const char* _str)
+{
+
+	int Temp = 0;
+
+	for (int i = 0; _str[i] != 0; ++i)
+	{
+		if (_str[i] >= '0' && _str[i] <='9')
+		{
+			_str[i] -= '0';
+
+		}
+
+	}
+
+
+}
+
+
+int main()
+{
+	int Value = ToInt("15815");
+}
