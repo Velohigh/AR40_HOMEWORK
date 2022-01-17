0262200b6064c7714a074d3e62d39f6449c5a52d
Velohigh <85592598+Velohigh@users.noreply.github.com>
2022-01-17 21:29:49 +0900

    정건효 : CountFirst, CountLast 과제 커밋

--- HomeWork220114_ToInt/HomeWork220114_ToInt.cpp
+++ HomeWork220114_ToInt/HomeWork220114_ToInt.cpp
@@ -6,21 +6,42 @@
 // 길이구하는 함수가 필요할듯..
 // 자리수에 
 
-int ToInt(const char* _str)
+int StringLen(const char* _Text)
+{
+	int Count = 0;
+	for (int i = 0; _Text[i] != 0; ++i)
+	{
+		++Count;
+	}
+
+	return Count;
+}
+
+int ToInt(const char* _Text)
 {
 
 	int Temp = 0;
+	int Value = 1;
+	int Mulcount = 0;
+	int StringLength = StringLen(_Text);
 
-	for (int i = 0; _str[i] != 0; ++i)
+	Mulcount = StringLength;
+
+	for (int i = 0; i < StringLength; ++i)
 	{
-		if (_str[i] >= '0' && _str[i] <='9')
-		{
-			_str[i] -= '0';
+		int CurValue = _Text[i] - '0';
+		Mulcount = StringLength - i -1;
 
+		while (Mulcount != 0)
+		{
+			--Mulcount;
+			CurValue *= 10;
 		}
+		Temp += CurValue;
 
 	}
 
+	return Temp;
 
 }
 
@@ -28,4 +49,6 @@ int ToInt(const char* _str)
 int main()
 {
 	int Value = ToInt("15815");
+
+	int a = 0;
 }
---
+++ HomeWork220117_/HomeWork220117_.cpp
@@ -0,0 +1,48 @@
+﻿// HomeWork220117_.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    for (int i = _Start; 0 != _Text[i]; ++i)
+    {
+        if (_Text[i] == _FindStr[0] &&  _Text[i+1] == _FindStr[1] && _Text[i+2] == _FindStr[2])
+        {
+            return i;
+        }
+
+    }
+
+    return -1;
+}
+
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+
+    for (int i = _End; _Text[i] != 0; --i)
+    {
+        if (_Text[i] == _FindStr[0] && _Text[i + 1] == _FindStr[1] && _Text[i + 2] == _FindStr[2])
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+int main()
+{
+    {
+        int Count = CountFirst("aaa eee ttt asdfasd eee", 0, "eee");
+
+        int a = 0;
+    }
+    {
+        int Count = CountLast("aaa eee asdfasd eee", 19, "eee");
+
+        int a = 0;
+    }
+
+}
